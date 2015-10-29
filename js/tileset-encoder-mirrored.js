7f79be48137e5d56d2dfa48712d0cb5d0fa53089
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2015-10-29 20:38:29 -0200

    Implemented the encoder for mirrored tiles

--- js/tileset-encoder-mirrored.js
+++ js/tileset-encoder-mirrored.js
@@ -13,9 +13,38 @@
 		doEncoding: function(converted, originalVideo) {
 			converted.format = 'TILE_MAPVH';
 			
-			var tileMap = {};
-			function convertTile() {
+			var convertedTileCache = {};
+			function convertTile(hex) {
+				if (convertedTileCache[hex]) {
+					return convertedTileCache[hex];
+				}
 				
+				var originalTile = TileSet.hexToTile(hex);
+				var candidates = Util.cartesianProductOf([false, true], [false, true], [false, true]).map(function(tuple){
+					var candidate = {};
+					var transformedTile = Util.deepClone(originalTile);
+					
+					if (tuple[0]) {
+						candidate.h = 1;
+						transformedTile = TileSet.flipH(transformedTile);
+					}
+					
+					if (tuple[1]) {
+						candidate.v = 1;
+						transformedTile = TileSet.flipV(transformedTile);
+					}
+					
+					if (tuple[2]) {
+						candidate.i = 1;
+						transformedTile = TileSet.inverseColor(transformedTile);
+					}
+					
+					candidate.hex = TileSet.tileToHex(transformedTile);
+					return candidate;
+				});
+				
+				convertedTileCache[hex] = _.min(candidates, 'hex');				
+				return convertedTileCache[hex];
 			}
 			
 			converted.frames = originalVideo.frames.map(function(origFrame){
@@ -27,11 +56,12 @@
 				
 				var tileNumbers = {};
 				origFrame.tiles.forEach(function(encodedTile, index){
-					var tileNumber = tileNumbers[encodedTile];
+					var convertedTile = convertTile(encodedTile);
+					var tileNumber = tileNumbers[convertedTile.hex];
 					if (_.isUndefined(tileNumber)) {
 						tileNumber = destFrame.tiles.length;
-						destFrame.tiles.push(encodedTile);
-						tileNumbers[encodedTile] = tileNumber;
+						destFrame.tiles.push(convertedTile.hex);
+						tileNumbers[convertedTile.hex] = tileNumber;
 					}
 					
 					var tx = index % originalVideo.tileCountX;
@@ -39,7 +69,7 @@
 					if (!destFrame.map[ty]) {
 						destFrame.map[ty] = [];
 					}
-					destFrame.map[ty][tx] = tileNumber;
+					destFrame.map[ty][tx] = _.extend({n: tileNumber}, _.pick(convertedTile, 'v', 'h', 'i'));
 				});
 				
 				return destFrame;
--- js/util.js
+++ js/util.js
@@ -12,7 +12,23 @@ window.Util = {
 			a[ln].push(n); 
 			return a 
 		}, []);
+	},
+	
+	/** Based on: http://stackoverflow.com/a/12628791/679240 */
+	cartesianProductOf: function() {
+		return _.reduce(arguments, function(a, b) {
+			return _.flatten(_.map(a, function(x) {
+				return _.map(b, function(y) {
+					return x.concat([y]);
+				});
+			}), true);
+		}, [ [] ]);
+	},
+	
+	deepClone: function(o) {
+		return JSON.parse(JSON.stringify(o));
 	}
+
 }
 
 })();
