03398d839a8570d452bd0cb86c48fa8b238455cf
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2015-11-07 21:51:47 -0200

    Encoding map streams

--- encode-binary/to-binary.js
+++ encode-binary/to-binary.js
@@ -28,6 +28,31 @@ wr.writeChar('*')
 	.writeWord(originalVideo.frames.length);
 	
 var TILEDATA_TERMINATOR = 0xFFFF;
+var MAP_COMMANDS = {
+	// Copy the next n bytes, unmodified (VERBATIM)
+	'v': 0x00,
+	// Repeat the next byte n times (REPEAT)
+	'r': 0x40,
+	// Skip the next n bytes (SKIP)
+	's': 0x80,
+	// Sets the top bits of the length of the next command (BOOST_LENGTH)
+	'b': 0xC0,
+	// End this frame (END)
+	'e': 0xFF
+};
+var MAP_COUNT_MASK = 0x3F;
+
+function outputMapCommands(commands, formatter) {
+	commands.forEach(function(command){
+		if (command.c) {
+			var prefix = MAP_COMMANDS[command.c];
+			var suffix = (command.l || 0) & MAP_COUNT_MASK;
+			wr.writeByte(prefix | suffix);
+		} else {
+			wr.writeByte(formatter(command));
+		}
+	});	
+}
 	
 originalVideo.frames.forEach(function(frame){
 	wr.writeChar('F');
@@ -47,6 +72,17 @@ originalVideo.frames.forEach(function(frame){
 		});
 	}
 	wr.writeWord(TILEDATA_TERMINATOR);
+	
+	// Map Delta-RLE
+	outputMapCommands(frame.map.tiles, function(command){
+		return command.n;
+	});
+	outputMapCommands(frame.map.attrs, function(command){
+		return (command.t ? 0x01 : 0) | // Is the 9th bit of tile number set?
+			(command.h ? 0x02 : 0) | // Is the tile horizontally flipped?
+			(command.v ? 0x04 : 0) | // Is the tile vertically flipped?
+			(command.i ? 0x08 : 0); // Is the sprite palette being used?
+	});
 });
 
 fs.writeFile('encoded-video-20x15-30fps-deltarle.bin', wr.toBuffer());
