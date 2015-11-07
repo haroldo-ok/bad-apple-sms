4c7378571b0ac39e86b92699c03621610eacccba
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2015-11-07 21:06:46 -0200

    Encoding tiles per frame

--- encode-binary/to-binary.js
+++ encode-binary/to-binary.js
@@ -22,4 +22,31 @@ originalVideo.tileBank.forEach(function(tile){
 	wr.writeHex(tile);
 });
 
+// Frames
+
+wr.writeChar('*')
+	.writeWord(originalVideo.frames.length);
+	
+var TILEDATA_TERMINATOR = 0xFFFF;
+	
+originalVideo.frames.forEach(function(frame){
+	wr.writeChar('F');
+	
+	// Tiles from tilebank
+	if (frame.tilesFromBank) {
+		frame.tilesFromBank.forEach(function(tile){
+			wr.writeWord(tile.dest).writeWord(tile.orig);
+		});
+	}
+	wr.writeWord(TILEDATA_TERMINATOR);
+
+	// Unique tiles included as part of the stream
+	if (frame.tilesToStream) {
+		frame.tilesToStream.forEach(function(tile){
+			wr.writeWord(tile.dest).writeHex(tile.tile);
+		});
+	}
+	wr.writeWord(TILEDATA_TERMINATOR);
+});
+
 fs.writeFile('encoded-video-20x15-30fps-deltarle.bin', wr.toBuffer());
